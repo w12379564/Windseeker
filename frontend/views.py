2db5f6b92fd381526cde8fd525d6dadf875b3861
w12379564 <imganyuxiang@gmail.com>
2017-09-12 21:03:26 +0800

    add templates & static files

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'website/index.html', context={
+                      'title': '命运石之门的选择',
+                      'welcome': 'Welcome to Steins;Gate'
+                  })
+
+def charts(request):
+    return render(request, 'website/charts.html')
+
+def tables(request):
+    return render(request, 'website/tables.html')
+
+def search_ajax(request):
+    ret=[12,11,10,9,8,7,6,5,4,3,2,1]
+    return JsonResponse(ret,safe=False)
