2eca98df9f3524a7a8d985165e308fc00351581b
w12379564 <imganyuxiang@gmail.com>
2017-09-12 20:44:34 +0800

    edit urls

---
+++ frontend/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'index', views.index, name='index'),
+    url(r'charts', views.charts, name='charts'),
+    url(r'tables', views.tables, name='tables'),
+    url(r'^search_ajax/$', views.search_ajax, name='search_ajax')
+]
