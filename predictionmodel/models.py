5dd3e381c9f869b92db6f242cf41cb5847d37d64
w12379564 <imganyuxiang@gmail.com>
2017-09-13 11:50:14 +0800

    add model

--- predictionmodel/models.py
+++ predictionmodel/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class weathertest(models.Model):
+    time = models.DateTimeField(primary_key=True)
+    temp = models.FloatField()
+    hum = models.FloatField()
+    press = models.FloatField()
+    dir = models.CharField(max_length=50)
+    windspeed = models.FloatField()
+    condition = models.CharField(max_length=50)
