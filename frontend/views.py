fc8ad87161a38e332e579d98afa74d25c0cbea68
w12379564 <imganyuxiang@gmail.com>
2017-09-16 21:03:13 +0800

    test

--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from datetime import datetime
 from predictionmodel.dataQuery import getrealBytime,getpredictBytime
+from predictionmodel.tasks import predictTask
 
 # Create your views here.
 def index(request):
@@ -17,6 +18,7 @@ def tables(request):
     return render(request, 'website/tables.html')
 
 def search_ajax(request):
+    predictTask()
     beg=request.GET.get('beg',0)
     end=request.GET.get('end',0)
     begtime = datetime.strptime(beg,'%Y/%m/%d %H:%M')
